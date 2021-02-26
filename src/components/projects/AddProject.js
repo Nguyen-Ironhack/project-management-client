d12a16dfff45ab4f811143ef7f0141a896ada6df
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-26 12:39:54 +0100

    project detail

--- src/components/projects/AddProject.js
+++ src/components/projects/AddProject.js
@@ -11,7 +11,7 @@ class AddProject extends Component {
     axios
       .post("http://localhost:5000/api/projects", { title, description })
       .then(() => {
-        // this.props.getData();
+        this.props.getData();
         this.setState({ title: "", description: "" });
       })
       .catch((error) => console.log(error));
