53835ec3cf83767411f2e4e747584a40b6735947
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-26 12:44:43 +0100

    EditProject components

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link, Redirect } from 'react-router-dom';
- 
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 class ProjectDetails extends Component {
-  state = {}
- 
-  render(){
-    return <div>Welcome to project details page!</div>
+  state = {};
+
+  componentDidMount() {
+    this.getSingleProject();
+  }
+
+  getSingleProject = () => {
+    const { params } = this.props.match;
+    axios
+      .get(`http://localhost:5000/api/projects/${params.id}`)
+      .then((responseFromApi) => {
+        const theProject = responseFromApi.data;
+        this.setState(theProject);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.description}</p>
+        <Link to={"/projects"}>Back to projects</Link>
+      </div>
+    );
   }
 }
- 
+
 export default ProjectDetails;
